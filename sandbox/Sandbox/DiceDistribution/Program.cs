3c0d265571f3337cf9027fcbc600e2f1e7de8936
Corobr <122401467+Corobr@users.noreply.github.com>
2025-09-24 09:37:52 -0600

    Prep 1

---
+++ sandbox/Sandbox/DiceDistribution/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DiceDistribution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> tallies = new List<int>();
+        PrepareTallyList(tallies);
+
+        int experiments = 1000000;
+        ThrowLotsOfDice(tallies, experiments);
+
+        DisplayResults(tallies, experiments);
+    }
+}
