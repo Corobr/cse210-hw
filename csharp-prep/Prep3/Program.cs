bb9cb4859b7fb05294f71aa7b03d7c3b27602d18
Corobr <122401467+Corobr@users.noreply.github.com>
2025-09-24 14:41:10 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        Random randomGenerator = new Random();
+        int input = randomGenerator.Next(1, 11);
+
+        while (guess != input)
+        {
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == input)
+            {
+                Console.WriteLine("You got it!");
+            }
+            else if (guess > input)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
